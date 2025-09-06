337f1e3e1f68ac8b7e61a8889fb2f3ffb64dae85
Piyas Sarkar <piyassarkar2006@gmail.com>
2025-09-06 23:03:33 +0530

    Add files via upload

---
+++ Roll_100.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("Enter the Roll no.:");
+	scanf("%d",&x);
+	if (x==100)
+    {
+        printf("Roll no. 100 is present");
+    }
+   
+    else
+    {
+        printf("Roll no. 100 is not present");
+    }
+
+   
+	return 0;
+}
