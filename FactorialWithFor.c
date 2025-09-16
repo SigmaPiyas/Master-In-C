705b57de1effdcbdf38daf983bbede4e4c26c166
Piyas Sarkar <piyassarkar2006@gmail.com>
2025-09-16 16:16:39 +0530

    FactorialWithFor.c

--- FactorialWithFor.c
+++ FactorialWithFor.c
@@ -8,12 +8,7 @@ int main()
     printf("Enter a non-negative integer: ");
     scanf("%d", &n);
 
-    if (n < 0)
-    {
-        printf("Factorial is not defined for negative numbers.\n");
-    }
-
-    else if (n == 0)
+    if (n == 0)
     {
         printf("Factorial of 0 is 1.\n");
     }
@@ -28,4 +23,5 @@ int main()
     }
 
     return 0;
+
 }
