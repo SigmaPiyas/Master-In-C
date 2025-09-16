6e4f7f4fde92f711b490461271aeff85f9c6ca5f
Piyas Sarkar <piyassarkar2006@gmail.com>
2025-09-16 15:47:47 +0530

    Prime.c

--- Prime.c
+++ Prime.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
-
-int Prime(int n) {
-    
-    for (int i = 2; i * i <= n; i++) { 
-        if (n % i == 0) {
-            return 0; 
-        }
-    }
-    return 1; 
-}
-
 int main() {
-    int start, end;
-    printf("Enter the starting number of the range: ");
-    scanf("%d", &start);
-    printf("Enter the ending number of the range: ");
-    scanf("%d", &end);
-    for (int i = start; i <= end; i++) {
-        if (Prime(i)) {
-             printf("Prime numbers between %d and %d are: %d\n", start, end,i);
-             
+    int lower, upper, i, j, isPrime;
+
+    printf("Enter lower limit: ");
+    scanf("%d", &lower);
+    printf("Enter upper limit: ");
+    scanf("%d", &upper);
+
+    printf("Prime numbers between %d and %d are: ", lower, upper);
+
+    for (i = lower; i <= upper; i++) {
+        if (i < 2) {
+            continue;
+        }
+
+        isPrime = 1;
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime == 1) {
+            printf("%d ", i);
         }
     }
-    
+
+    printf("\n");
     return 0;
 }
